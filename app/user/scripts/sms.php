51f963cbdcae1211819f2df4fadb7c710f5465aa
Blount <blount@ilatumi.org>
2014-06-21 17:15:29 +0200

    finaliser la configuration de la notification SMS.

--- app/user/scripts/sms.php
+++ app/user/scripts/sms.php
@@ -1,20 +1,12 @@
 <?php
 
-$params = array(
-    "free_mobile_user" => "", "free_mobile_key" => ""
-);
+require_once DOCUMENT_ROOT."/app/models/User/Storage.php";
+require_once "SMS/FreeMobile.php";
 
-$dir = DOCUMENT_ROOT.DS."var".DS."configs";
-if (!is_dir($dir)) {
-    mkdir($dir);
-}
-$filename = $dir.DS."user_".$auth->getUsername().".json";
-if (is_file($filename)) {
-    $data = json_decode(trim(file_get_contents($filename)), true);
-    if ($data && is_array($data)) {
-        $params = array_merge($params, array_intersect_key($data, $params));
-    }
-}
+$params = array(
+    "free_mobile_user" => (string) $userAuthed->getOption("free_mobile_user"),
+    "free_mobile_key" => (string) $userAuthed->getOption("free_mobile_key")
+);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST["free_mobile_user"])) {
@@ -26,18 +18,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($errors)) {
         $params = array_merge($params, array_intersect_key($_POST, $params));
         if (!empty($_POST["test"])) {
-//             var_dump("https://smsapi.free-mobile.fr/sendmsg?".
-//                 "user=".$params["free_mobile_user"].
-//                 "&pass=".$params["free_mobile_key"].
-//                 "&msg=Alerte"); exit;
-            $ch = curl_init("https://smsapi.free-mobile.fr/sendmsg?".
-                "user=".$params["free_mobile_user"].
-                "&pass=".$params["free_mobile_key"].
-                "&msg=Alerte");
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-            var_dump(curl_exec($ch)); exit;
+            $sms = new \SMS\FreeMobile();
+            $sms->setKey($params["free_mobile_key"])
+                ->setUser($params["free_mobile_user"]);
+            try {
+//                 $sms->send("La notification SMS est fonctionnelle.");
+            } catch (Exception $e) {
+                $error = "Erreur lors de l'envoi du SMS: (".$e->getCode().") ".$e->getMessage();
+            }
         } else {
-            file_put_contents($filename, json_encode($params));
+            $userAuthed->setOption("free_mobile_user", $params["free_mobile_user"])
+                ->setOption("free_mobile_key", $params["free_mobile_key"]);
+            $userStorage->save($userAuthed);
             header("LOCATION: ./?mod=user&a=sms"); exit;
         }
     }
