1d6b23304a8b4f8e476fedb1c56b6b2223cd0f21
Blount <blount@ilatumi.org>
2016-04-19 21:45:15 +0200

    migrer les URLs http://www.leboncoin.fr vers https://www.leboncoin.fr

--- others/update/3.2.php
+++ others/update/3.2.php
@@ -8,6 +8,23 @@ class Update_32 extends Update
     {
         if ("db" == $this->_storage) {
             $this->_dbConnection->query("ALTER TABLE `LBC_Alert` CHANGE `last_id` `last_id` TEXT NULL DEFAULT NULL");
+            $this->_dbConnection->query('UPDATE `LBC_Alert` SET `url` = REPLACE(`url`, "http://www.leboncoin.fr/", "https://www.leboncoin.fr/")');
+
+        } elseif ("files" == $this->_storage) {
+            $dir = DOCUMENT_ROOT.DS."var".DS."configs";
+            if (is_dir($dir)) {
+                $files = glob($dir.DS."*.csv");
+                foreach ($files AS $file) {
+                    file_put_contents(
+                        $file,
+                        str_replace(
+                            "http://www.leboncoin.fr",
+                            "https://www.leboncoin.fr",
+                            file_get_contents($file)
+                        )
+                    );
+                }
+            }
         }
     }
 }
