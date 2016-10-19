bd01f937f5dc8d6421f144fae798e12176955b30
Blount <blount@ilatumi.org>
2016-10-19 10:24:39 +0200

    Ajoute une confirmation lors de la sauvegarde d'une annonce existante

--- app/annonce/scripts/backup.php
+++ app/annonce/scripts/backup.php
@@ -18,10 +18,25 @@ $ad = $parser->processAd(
 );
 
 $ad_stored = $storage->fetchById($ad->getId());
-if (!$ad_stored) {
-    $ad_stored = new \App\BackupAd\Ad();
-    $ad_stored->setFromArray($ad->toArray());
+if ($ad_stored) {
+    if ($_SERVER["REQUEST_METHOD"] != "POST") {
+        return;
+    }
+
+    // Supprime les photos
+    foreach ($ad->getPhotos() AS $photo) {
+        $filename = DOCUMENT_ROOT."/static/media/annonce/".$photo["local"];
+        if (is_file($filename)) {
+            unlink($filename);
+        }
+    }
 }
 
+
+if (!$ad_stored) {
+    $ad_stored = new \App\BackupAd\Ad();
+}
+
+$ad_stored->setFromArray($ad->toArray());
 $storage->save($ad_stored);
 header("LOCATION: ./?mod=annonce&a=view&id=".$ad->getId()); exit;
