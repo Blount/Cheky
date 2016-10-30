248026e938c3f730f954b8ca98988b23b1134ac0
Blount <blount@ilatumi.org>
2016-10-30 22:25:41 +0100

    Ajoute une constante pour gérer le cache des fichiers statiques

--- bootstrap.php
+++ bootstrap.php
@@ -10,6 +10,10 @@ defined("APPLICATION_ENV")
 // Define application environment
 define("APPLICATION_VERSION", require DOCUMENT_ROOT."/version.php");
 
+// Constante utilisée pour forcer le navigateur à télécharger
+// les fichiers statiques (js, css, html, etc)
+define("STATIC_REV", 10);
+
 set_include_path(
     __DIR__."/lib".PATH_SEPARATOR.get_include_path()
 );
