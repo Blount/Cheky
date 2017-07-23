b3015f57b9721c9443abdca839c46ff4d244b0e6
Blount <blount@ilatumi.org>
2017-07-23 17:31:38 +0200

    Ajoute un contrôle de sécurité sur le fichier de configuration (fixes #65)

--- app/admin/init.php
+++ app/admin/init.php
@@ -4,3 +4,15 @@ if ($auth->getUsername() != "admin") {
     header("HTTP/1.1 403 Forbidden");
     exit;
 }
+
+$security_issue = false;
+
+$test_url = $config->get("general", "baseurl")."/var/config.ini";
+$content = $client->request($test_url);
+
+if (200 == $client->getRespondCode()
+    || false !== strpos($content, "[general]")) {
+    $security_issue = true;
+}
+
+unset($test_url, $content);
